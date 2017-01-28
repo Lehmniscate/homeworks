e53347ca2baa5759430f00665272614e73229d29
Lehmniscate <mchlhtn@gmail.com>
2017-01-27 20:34:10 -0800

    updated the game to satisfy the rspec tests

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,19 +4,30 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over
+      @sequence_length += 1
+      round_success_message
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +35,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.shuffle.first
   end
 
   def round_success_message
@@ -36,6 +47,8 @@ class Simon
   end
 
   def reset_game
-
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
   end
 end
