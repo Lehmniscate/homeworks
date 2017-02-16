4ef78216f289cba8efea108b35e6c1b8208a1ef8
Lehmniscate <mchlhtn@gmail.com>
2017-02-15 17:39:17 -0800

    added authorization token helper method

--- W4D4/app/helpers/application_helper.rb
+++ W4D4/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def auth_token
+    html = "<input type='hidden' name='authenticity_token'value='".html_safe
+    html += form_authenticity_token.html_safe
+    html += "'>'".html_safe
+    html
+  end
+
 end
