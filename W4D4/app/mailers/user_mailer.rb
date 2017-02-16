53d127b1da55a0bf691452478be04f6b31c8acfe
Lehmniscate <mchlhtn@gmail.com>
2017-02-15 18:31:56 -0800

    added welcome emails

--- W4D4/app/mailers/user_mailer.rb
+++ W4D4/app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'everybody@appacademy.io'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: user.email, subject: 'Welcome!!!')
+  end
 end
